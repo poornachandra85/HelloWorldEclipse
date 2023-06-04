529cf224ee04b126ad42bcb923c1bbafc558a84b
POORNACHANDRA K H <poornachandrakh@gmail.com>
2023-06-04 17:53:12 +0530

    Default packing Helloworls

---
+++ LearnIT - Fundamentals/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * Hello World Application
+ * @author Admin
+ *
+ */
+public class HelloWorld {
+	/**
+	 * the point to program
+	 * @param args
+	 */
+	public static void main(String[] args) {		
+		System.out.println("Hello World!");
+	}
+
+}
